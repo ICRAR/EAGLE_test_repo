138c85453c8b7c0d270e4a0e45fc951675243a9f
Ryan Bunney <ryanbunney@fastmail.com>
2025-09-25 14:42:05 +0800

    Release: version v0.2.1 🚀

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools.command.install import install
 
 setup(
     name="eagle-test-graphs",
-    version="0.2.0",
+    version="0.2.1",
     packages=find_packages(where="eagle_test_graphs"),
     package_dir={"": "eagle_test_graphs"},
     package_data={"": ["*.graph", "*.pkl", "*.json", "*.spec"]},
